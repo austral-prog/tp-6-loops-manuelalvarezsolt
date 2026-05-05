499a659dcfaa9287ebd7a5ce2b6b04663aae528d
manuelalvarezsolt <manuel.alvarez.solt@gmail.com>
2026-05-05 12:46:01 -0300

    Update list_modify.py

--- list_modify.py
+++ list_modify.py
@@ -12,18 +12,31 @@ def put(value, lst):
         put("Blue", colors) -> 1
         # colors ahora es ["Red", "Blue", "Green"]
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if "" in lst:
+        m = lst.index("")
+        lst[m] = value
+        return m
+    else:
+        return -1
+
+
 
 
 def remove(value, lst):
-    """
-    Busca todas las ocurrencias de value en lst, las reemplaza por ""
-    y retorna la cantidad de eliminaciones realizadas.
-    IMPORTANTE: esta funcion modifica la lista original.
+        """
+        Busca todas las ocurrencias de value en lst, las reemplaza por ""
+        y retorna la cantidad de eliminaciones realizadas.
+        IMPORTANTE: esta funcion modifica la lista original.
 
-    Ejemplo:
-        colors = ["Red", "Green", "Red", "Blue"]
-        remove("Red", colors) -> 2
-        # colors ahora es ["", "Green", "", "Blue"]
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+        Ejemplo:
+            colors = ["Red", "Green", "Red", "Blue"]
+            remove("Red", colors) -> 2
+            # colors ahora es ["", "Green", "", "Blue"]
+        """
+        m = 0
+        for element in range(len(lst)):
+            if value == lst[element]:
+                lst[element] = ""
+                m = m + 1
+        return m
+
